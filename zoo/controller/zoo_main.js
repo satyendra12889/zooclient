5278893bbaf9bad62116751f5d490cfa22b98171
satyendra <satyendransit12@gmail.com>
2020-05-04 18:07:16 +0530

    zoo server first draft.

--- zoo/controller/zoo_main.js
+++ zoo/controller/zoo_main.js
@@ -2,17 +2,30 @@ var ZooController = function($scope, $http, $state) {
    
    $scope.areas = [];
    $scope.stock = [];
+   $scope.calledFromChange = false;
 
 
 
    $scope.name = "hi";
 
-   $http({
+
+ $scope.getDashBoardData = function(){
+ 	  $http({
   method: 'GET',
-  url: 'http://localhost:8081/zoo-management/dashboard'
+  url: 'http://localhost:8081/zoo-management/dashboard',
+  params:{
+  	animalType:($scope.selectedAnimal!=null?$scope.selectedAnimal.codeId:-1),
+  	areaId:($scope.selectedArea!=null?$scope.selectedArea.areaId:-1),
+  	penId:($scope.selectedpen!=null?$scope.selectedpen.pegId:-1)
+  }
+
 }).then(function successCallback(response) {
      console.log(response);
+   if(!$scope.calledFromChange){
     $scope.areas = response.data.allArea;
+    $scope.pen = [];
+}
+$scope.calledFromChange = false;
     $scope.animal = response.data.animal;
   }, function errorCallback(response) {
     console.log(response);
@@ -20,19 +33,68 @@ var ZooController = function($scope, $http, $state) {
     // or server returns response with an error status.
   });
 
+}
 
+$scope.changesArea = function($event, x){
+	$scope.calledFromChange = true;
+	$scope.getPenForArea();
+	$scope.getDashBoardData();
+}
+
+$scope.getPenForArea = function(){
+	if($scope.selectedArea!=null){
+	  $http({
+	
+  method: 'GET',
+  url: 'http://localhost:8081/zoo-management/pen/filled/'+$scope.selectedArea.areaId
+ 
+
+}).then(function successCallback(response) {
+     console.log(response);
+    $scope.pen = response.data;
+    
+  }, function errorCallback(response) {
+    console.log(response);
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+
+}
+}
+
+
+$scope.reset = function(){
+	$scope.selectedAnimal = null;
+	$scope.selectedArea = null;
+	$scope.selectedpen = null;
+	$scope.getDashBoardData();
+}
  $http({
   method: 'GET',
   url: "http://localhost:8081/zoo-management/animal/type/list"
 }).then(function successCallback(response) {
      console.log(response);
     $scope.animalType = response.data;
+    // $scope.pen = null;
+    $scope.selectedpen = null;
+
   }, function errorCallback(response) {
     console.log(response);
     
     });
 
+	$scope.penClicked = function($event, data)
+	{
+		$scope.calledFromChange = true;
+		$scope.getDashBoardData();
 
+	}
+
+	$scope.animalTypeClicked = function($event, data)
+	{
+		$scope.calledFromChange = true;
+		$scope.getDashBoardData();
+	}
 
    $scope.areaClicked = function($event, data){
    	$state.go('areadetail', {areaId: data.areaId});
@@ -49,6 +111,8 @@ var ZooController = function($scope, $http, $state) {
    $scope.addAreaClicked = function(){
    	$state.go('addarea', {});
    }
+
+   $scope.getDashBoardData();
 };
 
 ZooController.$inject = ['$scope', '$http','$state'];
